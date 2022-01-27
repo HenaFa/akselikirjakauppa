c6ef1c90d76a3cb1dccc9566b308c0446f39b6c6
hirvaks <akseli.hirvela@gmail.com>
2022-01-27 14:55:36 +0200

    first commit

---
+++ src/main/java/com/example/akselikirjakauppa/AkselikirjakauppaApplication.java
@@ -0,0 +1,13 @@
+package com.example.akselikirjakauppa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkselikirjakauppaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkselikirjakauppaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/akselikirjakauppa/web/BookController.java
@@ -0,0 +1,13 @@
+package com.example.akselikirjakauppa.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+	@RequestMapping("/index")
+	public String viewBooks() {
+		return "";
+	}
+
+}
